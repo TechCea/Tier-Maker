7d45f73a60cbf7461ce5ae5d2ae0b500ae71e56b
BJaviersito <losthyghost@gmail.com>
2024-08-13 14:14:35 -0600

    it!!!!!!!! woorksss!

--- JS/Tier.js
+++ JS/Tier.js
@@ -1,160 +1,148 @@
-const $ = el => document.querySelector(el)
-    const $$ = el => document.querySelectorAll(el)
+const $ = el => document.querySelector(el);
+const $$ = el => document.querySelectorAll(el);
 
-    const imageInput = $('#image-input')
-    const itemsSection = $('#selector-items')
-    const resetButton = $('#reset-tier-button')
-    const saveButton = $('#save-tier-button')
+const dropArea = $('#drop-area');
+const itemsSection = $('#selector-items');
+const imageInput = $('#image-input');
+const resetButton = $('#reset-tier-button');
+const saveButton = $('#save-tier-button');
 
-    function createItem(src) {
-      const imgElement = document.createElement('img')
-      imgElement.draggable = true
-      imgElement.src = src
-      imgElement.className = 'item-image'
+let draggedElement = null;
+let sourceContainer = null;
 
-      imgElement.addEventListener('dragstart', handleDragStart)
-      imgElement.addEventListener('dragend', handleDragEnd)
+dropArea.addEventListener('dragover', (event) => {
+    event.preventDefault();
+    dropArea.classList.add('drag-over');
+});
 
-      itemsSection.appendChild(imgElement)
-      return imgElement
-    }
+dropArea.addEventListener('dragleave', () => {
+    dropArea.classList.remove('drag-over');
+});
 
-    function useFilesToCreateItems(files) {
-      if (files && files.length > 0) {
+dropArea.addEventListener('drop', (event) => {
+    event.preventDefault();
+    dropArea.classList.remove('drag-over');
+
+    const { files } = event.dataTransfer;
+    useFilesToCreateItems(files);
+});
+
+function createItem(src, container) {
+    const imgElement = document.createElement('img');
+    imgElement.draggable = true;
+    imgElement.src = src;
+    imgElement.className = 'item-image';
+
+    imgElement.addEventListener('dragstart', handleDragStart);
+    imgElement.addEventListener('dragend', handleDragEnd);
+
+    container.appendChild(imgElement);
+}
+
+function useFilesToCreateItems(files) {
+    if (files && files.length > 0) {
         Array.from(files).forEach(file => {
-          const reader = new FileReader()
+            const reader = new FileReader();
 
-          reader.onload = (eventReader) => {
-            createItem(eventReader.target.result)
-          }
+            reader.onload = (eventReader) => {
+                createItem(eventReader.target.result, itemsSection);
+            };
 
-          reader.readAsDataURL(file)
-        })
-      }
+            reader.readAsDataURL(file);
+        });
+    }
+}
+
+imageInput.addEventListener('change', (event) => {
+    const { files } = event.target;
+    useFilesToCreateItems(files);
+});
+
+const rows = $$('.tier .row');
+
+rows.forEach(row => {
+    row.addEventListener('dragover', handleDragOver);
+    row.addEventListener('drop', handleDrop);
+    row.addEventListener('dragleave', handleDragLeave);
+});
+
+function handleDragStart(event) {
+    draggedElement = event.target;
+    sourceContainer = draggedElement.parentNode;
+    event.dataTransfer.setData('text/plain', draggedElement.src);
+}
+
+function handleDragEnd() {
+    draggedElement = null;
+    sourceContainer = null;
+}
+
+function handleDragOver(event) {
+    event.preventDefault();
+
+    const currentTarget = event.currentTarget;
+
+    if (sourceContainer === currentTarget) return;
+
+    currentTarget.classList.add('drag-over');
+
+    const dragPreview = document.querySelector('.drag-preview');
+
+    if (draggedElement && !dragPreview) {
+        const previewElement = draggedElement.cloneNode(true);
+        previewElement.classList.add('drag-preview');
+        currentTarget.appendChild(previewElement);
+    }
+}
+
+function handleDrop(event) {
+    event.preventDefault();
+
+    const currentTarget = event.currentTarget;
+
+    if (sourceContainer && draggedElement) {
+        sourceContainer.removeChild(draggedElement);
     }
 
-    imageInput.addEventListener('change', (event) => {
-      const { files } = event.target
-      useFilesToCreateItems(files)
-    })
-
-    let draggedElement = null
-    let sourceContainer = null
-
-    const rows = $$('.tier .row')
-
-    rows.forEach(row => {
-      row.addEventListener('dragover', handleDragOver)
-      row.addEventListener('drop', handleDrop)
-      row.addEventListener('dragleave', handleDragLeave)
-    })
-
-    itemsSection.addEventListener('dragover', handleDragOver)
-    itemsSection.addEventListener('drop', handleDrop)
-    itemsSection.addEventListener('dragleave', handleDragLeave)
-
-    itemsSection.addEventListener('drop', handleDropFromDesktop)
-    itemsSection.addEventListener('dragover', handleDragOverFromDesktop)
-
-    function handleDragOverFromDesktop(event) {
-      event.preventDefault()
-
-      const { currentTarget, dataTransfer } = event
-
-      if (dataTransfer.types.includes('Files')) {
-        currentTarget.classList.add('drag-files')
-      }
+    if (draggedElement) {
+        currentTarget.appendChild(draggedElement);
     }
 
-    function handleDropFromDesktop(event) {
-      event.preventDefault()
-      const { currentTarget, dataTransfer } = event
+    currentTarget.classList.remove('drag-over');
+    currentTarget.querySelector('.drag-preview')?.remove();
+}
 
-      if (dataTransfer.types.includes('Files')) {
-        currentTarget.classList.remove('drag-files')
-        const { files } = dataTransfer
-        useFilesToCreateItems(files)
-      }
-    }
+function handleDragLeave(event) {
+    event.preventDefault();
 
-    function handleDrop(event) {
-      event.preventDefault()
+    const currentTarget = event.currentTarget;
+    currentTarget.classList.remove('drag-over');
+    currentTarget.querySelector('.drag-preview')?.remove();
+}
 
-      const { currentTarget, dataTransfer } = event
+resetButton.addEventListener('click', () => {
+    const items = $$('.tier .item-image');
+    items.forEach(item => {
+        item.remove();
+        itemsSection.appendChild(item);
+    });
+});
 
-      if (sourceContainer && draggedElement) {
-        sourceContainer.removeChild(draggedElement)
-      }
+saveButton.addEventListener('click', () => {
+    const tierContainer = $('.tier');
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
 
-      if (draggedElement) {
-        const src = dataTransfer.getData('text/plain')
-        const imgElement = createItem(src)
-        currentTarget.appendChild(imgElement)
-      }
-
-      currentTarget.classList.remove('drag-over')
-      currentTarget.querySelector('.drag-preview')?.remove()
-    }
-
-    function handleDragOver(event) {
-      event.preventDefault()
-
-      const { currentTarget, dataTransfer } = event
-      if (sourceContainer === currentTarget) return
-
-      currentTarget.classList.add('drag-over')
-
-      const dragPreview = document.querySelector('.drag-preview')
-
-      if (draggedElement && !dragPreview) {
-        const previewElement = draggedElement.cloneNode(true)
-        previewElement.classList.add('drag-preview')
-        currentTarget.appendChild(previewElement)
-      }
-    }
-
-    function handleDragLeave(event) {
-      event.preventDefault()
-
-      const { currentTarget } = event
-      currentTarget.classList.remove('drag-over')
-      currentTarget.querySelector('.drag-preview')?.remove()
-    }
-
-    function handleDragStart(event) {
-      draggedElement = event.target
-      sourceContainer = draggedElement.parentNode
-      event.dataTransfer.setData('text/plain', draggedElement.src)
-    }
-
-    function handleDragEnd(event) {
-      draggedElement = null
-      sourceContainer = null
-    }
-
-    resetButton.addEventListener('click', () => {
-      const items = $$('.tier .item-image')
-      items.forEach(item => {
-        item.remove()
-        itemsSection.appendChild(item)
-      })
-    })
-
-    saveButton.addEventListener('click', () => {
-      const tierContainer = $('.tier')
-      const canvas = document.createElement('canvas')
-      const ctx = canvas.getContext('2d')
-
-      import('https://cdn.jsdelivr.net/npm/html2canvas-pro@1.5.8/+esm')
+    import('https://cdn.jsdelivr.net/npm/html2canvas-pro@1.5.8/+esm')
         .then(({ default: html2canvas }) => {
-          html2canvas(tierContainer).then(canvas => {
-            ctx.drawImage(canvas, 0, 0)
-            const imgURL = canvas.toDataURL('image/png')
+            html2canvas(tierContainer).then(canvas => {
+                ctx.drawImage(canvas, 0, 0);
+                const imgURL = canvas.toDataURL('image/png');
 
-            const downloadLink = document.createElement('a')
-            downloadLink.download = 'tier.png'
-            downloadLink.href = imgURL
-            downloadLink.click()
-          })
-        })
-    })
+                const downloadLink = document.createElement('a');
+                downloadLink.download = 'tier.png';
+                downloadLink.href = imgURL;
+                downloadLink.click();
+            });
+        });
+});
