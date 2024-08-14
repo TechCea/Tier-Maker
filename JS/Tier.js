d9f3dbc28801f330780f1f95ff6e675ac04e20e6
BJaviersito <losthyghost@gmail.com>
2024-08-14 14:34:01 -0600

    colors

--- JS/Tier.js
+++ JS/Tier.js
@@ -146,3 +146,22 @@ saveButton.addEventListener('click', () => {
             });
         });
 });
+
+const colorPickers = document.querySelectorAll('.color-picker');
+const colorTriggers = document.querySelectorAll('.color-picker-trigger');
+
+colorTriggers.forEach((trigger, index) => {
+    trigger.addEventListener('click', () => {
+        const colorPicker = colorPickers[index];
+        colorPicker.click(); // Abre el selector de color
+    });
+});
+
+colorPickers.forEach((picker, index) => {
+    picker.addEventListener('input', (event) => {
+        const newColor = event.target.value;
+        const label = picker.closest('.label');
+        label.style.setProperty('--level', newColor);
+    });
+});
+
